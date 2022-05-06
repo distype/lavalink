ad1f82bdcdb1f3a5f5a39028f53111ae91103a2f
BR88C <br88c@br88c.dev>
2022-05-05 23:21:37 -0400

    Fix events not being propagated to player

--- src/structures/Manager.ts
+++ src/structures/Manager.ts
@@ -258,7 +258,7 @@ export class Manager extends TypedEmitter<ManagerEvents> {
 
     /**
      * Creates a new player and connects it to the voice channel. Also checks channel permissions.
-     * The player is not saved or bound to the manager if it fails to connect or doesn't have sufficient permissions.
+     * The player is not permanently saved or bound to the manager if it fails to connect or doesn't have sufficient permissions.
      * If a player for the specified guild already exists, it is returned and no new player is created. If it is disconnected, it is automatically connected.
      * @param guild The player's guild.
      * @param textChannel The player's text channel.
@@ -285,11 +285,12 @@ export class Manager extends TypedEmitter<ManagerEvents> {
         }
 
         const player = new Player(this, node, guild, textChannel, voiceChannel, options, this._log, this._logThisArg);
+        this.players.set(guild, player);
+
         await player.connect().finally(() => {
             if (player.state === PlayerState.DISCONNECTED) player.destroy();
         });
 
-        this.players.set(guild, player);
 
         player.on(`VOICE_CONNECTED`, (channel) => this.emit(`PLAYER_VOICE_CONNECTED`, player, channel));
         player.on(`VOICE_MOVED`, (newChannel) => this.emit(`PLAYER_VOICE_MOVED`, player, newChannel));
